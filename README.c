1f805b75f6674e7fe1fc8f2d87ad466b8e6c7582
aswini04 <aswini04081995@gmail.com>
2016-07-16 15:03:36 +0530

    finish

---
+++ README.c
@@ -0,0 +1,17 @@
+# ka
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+for(j=0;j<=i;j++)
+{
+printf("\n the loop is");
+}
+}
+}
+
